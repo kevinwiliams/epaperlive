99b888b2677df5d9107f2e4c785a74b079b8a4dd
Gregg Wong <griekup@live.com>
2022-11-07 19:58:24 -0500

    Change datatype

--- DBModel/subscriber_address.cs
+++ DBModel/subscriber_address.cs
@@ -10,7 +10,7 @@ namespace ePaperLive.DBModel
 
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AddressID { get; set; }
-        public int SubscriberID { get; set; }
+        public string SubscriberID { get; set; }
         public string AddressType { get; set; }
         public string EmailAddress { get; set; }
         public string AddressLine1 { get; set; }
@@ -20,9 +20,8 @@ namespace ePaperLive.DBModel
         public string ZipCode { get; set; }
         public string CountryCode { get; set; }
         public System.DateTime CreatedAt { get; set; }
-
         [ForeignKey("SubscriberID")]
         [Required]
-        public Subscriber Subscriber { get; set; }
+        public virtual Subscriber Subscriber { get; set; }
     }
 }
