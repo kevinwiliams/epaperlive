939e79f8f3003ac5b06529034ead32329f17f561
Gregg Wong <griekup@live.com>
2022-11-08 17:00:07 -0500

    Seed with roles

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 ﻿namespace ePaperLive.Migrations
 {
+    using Microsoft.AspNet.Identity.EntityFramework;
+
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +20,24 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            string admin = "Admin",
+             staff = "Staff",
+             subscriber = "Subscriber";
+
+            if (!context.Roles.Any(r => r.Name == admin))
+            {
+                context.Roles.Add(new IdentityRole() { Name = admin});
+            }
+
+            if (!context.Roles.Any(r => r.Name == staff))
+            {
+                context.Roles.Add(new IdentityRole() { Name = staff });
+            }
+
+            if (!context.Roles.Any(r => r.Name == subscriber))
+            {
+                context.Roles.Add(new IdentityRole() { Name = subscriber });
+            }
 
         }
     }
