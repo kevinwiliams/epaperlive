cc4932bfe7358b789d9f5787baba9bc87aa2037b
Gregg Wong <griekup@live.com>
2022-12-07 21:06:26 -0500

    Dictionary change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -98,14 +98,33 @@ namespace ePaperLive.Controllers
                                     .Where(x => x.Market == market)
                                     .Where(x => x.Active == true).ToList();
                 //ar pattrns = ratesList.Where(x => x.PrintDayPattern != "")
-                var daysOfWeek = new Dictionary<string, string>();
-                daysOfWeek.Add("SUN", "1");
-                daysOfWeek.Add("MON", "1");
-                daysOfWeek.Add("TUE", "1");
-                daysOfWeek.Add("WED", "1");
-                daysOfWeek.Add("THU", "1");
-                daysOfWeek.Add("FRI", "1");
-                daysOfWeek.Add("SAT", "1");
+                var daysOfWeek = new Dictionary<string, string>()
+                {
+                    ["SUN"] = "1",
+                    ["MON"] = "1",
+                    ["TUE"] = "1",
+                    ["WED"] = "1",
+                    ["THU"] = "1",
+                    ["FRI"] = "1",
+                    ["SAT"] = "1"
+                };
+                var daysOfWeek2 = new Dictionary<string, string>()
+                {
+                    { "SUN", "1" },
+                    { "MON", "1"},
+                    { "TUE", "1"},
+                    { "WED", "1"},
+                    { "THU", "1"},
+                    { "FRI", "1"},
+                    { "SAT", "1"}
+                };
+                //daysOfWeek.Add("SUN", "1");
+                //daysOfWeek.Add("MON", "1");
+                //daysOfWeek.Add("TUE", "1");
+                //daysOfWeek.Add("WED", "1");
+                //daysOfWeek.Add("THU", "1");
+                //daysOfWeek.Add("FRI", "1");
+                //daysOfWeek.Add("SAT", "1");
 
                 var newFrequency = "";
                 char[] charsToTrim = { ',', '.', ' ' };
