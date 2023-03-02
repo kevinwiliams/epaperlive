7214ac000e0d137e5180d04fb2a3f1cd17138bb9
Kevin WIlliams <kwilliams9285@gmail.com>
2023-03-02 14:04:33 -0500

    Fix Modal

---
+++ Scripts/site/_loginmodal.js
@@ -0,0 +1,8 @@
+﻿let _passwordInput = $("#PasswordModal");
+let _eye = $("#eyeModal");
+
+$(_eye).click(function () {
+    $(this).toggleClass("fa-eye-slash");
+    const type = $(_passwordInput).attr("type") === "password" ? "text" : "password";
+    $(_passwordInput).attr("type", type);
+});
