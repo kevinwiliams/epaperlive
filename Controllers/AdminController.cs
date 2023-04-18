9dd8f30304c6821cdc14aba5c909e8d805084945
Kevin WIlliams <kwilliams9285@gmail.com>
2023-04-18 18:09:49 -0500

    Add User Role

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using ePaperLive.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -10,6 +12,9 @@ using System.Web.Security;
 
 namespace ePaperLive.Controllers
 {
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("Admin")]
+    [Route("action = index")]
     public class AdminController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
@@ -19,7 +24,79 @@ namespace ePaperLive.Controllers
         {
             return View();
         }
-        
-       
+        [Route("addrole")]
+        public ActionResult AddRole()
+        {
+               List<RoleViewModel> roleViewModels = new List<RoleViewModel>();
+            
+            try
+            {
+                var userRoles = new SelectList(db.Roles.Where(u => !u.Name.Contains("Admins"))
+                                   .ToList(), "Name", "Name");
+
+                var uRoles = db.Roles.ToList();
+
+                foreach (var role in uRoles)
+                {
+                    var rvm = new RoleViewModel
+                    {
+                        Id = role.Id,
+                        Name = role.Name
+                    };
+                    roleViewModels.Add(rvm);
+                }
+
+                return View(roleViewModels);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Util.LogError(ex);
+                return View(roleViewModels);
+
+            }
+        }
+
+        [HttpPost]
+        [Route("addrole")]
+        public async Task<ActionResult> AddRole(string roleName)
+        {
+            List<RoleViewModel> roleViewModels = new List<RoleViewModel>();
+
+            try
+            {
+                using (var context = new ApplicationDbContext())
+                {
+                    var userStore = new UserStore<ApplicationUser>(context);
+                    var userManager = new ApplicationUserManager(userStore);
+                    // Create roles if the don't exist
+                    context.Roles.AddOrUpdate(r => r.Name, new IdentityRole() { Name = roleName });
+
+                    await context.SaveChangesAsync();
+
+                    foreach (var role in context.Roles.ToList())
+                    {
+                        var rvm = new RoleViewModel
+                        {
+                            Id = role.Id,
+                            Name = role.Name
+                        };
+                        roleViewModels.Add(rvm);
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Util.LogError(ex);
+            }
+
+            return View(roleViewModels);
+
+        }
     }
 }
